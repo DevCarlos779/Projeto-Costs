680302a1fc350a712fc525252c00d977de91339a
Carlos Emanuel <devcarlos0112@gmail.com>
2025-04-01 15:44:01 -0300

    Começando o Processo de edição de projetos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NewProject from "./components/pages/Newproject";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
+import Project from "./components/pages/Project";
 
 import Container from "./components/layout/Container";
 import NavBar from "./components/layout/NavBar";
@@ -23,7 +24,7 @@ function App() {
             <Route path="/projects" element={<Projects />}></Route>
             <Route path="/company" element={<Company />}></Route>
             <Route path="/contact" element={<Contact />}></Route>
-            <Route path="/newproject" element={<NewProject />}></Route>
+            <Route path="/projects/:id" element={<Project />}></Route>
         </Routes>
       </Container>
       
